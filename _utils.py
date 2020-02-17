8a5b6a9bd188cd79b3fa2e08bf4216157466c4ed
spencerwoo <spencer.woo@liferay.com>
2020-02-16 16:25:15 -0800

    add screenshot to _utils.py and refactor

--- _utils.py
+++ _utils.py
@@ -1,17 +1,30 @@
 import builtins
-import json
-import requests
 
 def print_flush(*objects, sep='', end='\n', flush=False):
     return builtins.print(objects, sep, end, flush=True)
 
 builtins.__print__ = print_flush
 
-def write_file(filename, content, write=True):
+def write_file(filename, content, write: bool=True):
     with open(filename,"w" if write else "a", encoding="UTF-8") as file:
         file.write(content)
+
+import json
+import requests
 
 def get_url(url: str):
     response = requests.get(url)
 
     return json.loads(response.text)
+
+import pyscreenshot
+import time
+
+def screenshot(min_x, min_y, max_x, max_y, filename: str=None):
+    box = (min_x, min_y, max_x, max_y)
+    im = pyscreenshot.grab(bbox=box)
+
+    if not filename:
+    	filename = str(int(time.time())) + '.png'
+
+    im.save(filename)
