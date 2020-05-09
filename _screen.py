9dd4beba82aa76783587c4d1e543557381e00d77
spencerwoo <spencer.woo@liferay.com>
2020-05-09 02:06:01 -0700

    test OCR

--- _screen.py
+++ _screen.py
@@ -9,6 +9,48 @@ import re
 
 from _utils import *
 
+def psm():
+    img_name = '1'
+    img = img_name + '.jpg'
+    for i in range(1,13):
+        text = pytesseract.image_to_string(img,lang='eng',config=r'--psm {i}')
+        write_file(f'{img_name}{i}.txt', text)
+
+def bounding_box_naive(image):
+    h, w, c = image.shape
+    boxes = pytesseract.image_to_boxes(image) 
+    for b in boxes.splitlines():
+        b = b.split(' ')
+        image = cv2.rectangle(image, (int(b[1]), h - int(b[2])), (int(b[3]), h - int(b[4])), (0, 255, 0), 2)
+
+    b,g,r = cv2.split(image)
+    rgb_img = cv2.merge([r,g,b])
+
+    plt.figure(figsize=(16,12))
+    plt.imshow(rgb_img)
+    plt.title('NAIVE BOUNDING BOX RECOGNITION')
+    plt.show()
+
+def bounding_box(image):
+    d = pytesseract.image_to_data(image, output_type=Output.DICT)
+
+    n_boxes = len(d['text'])
+    valid_pattern = '^(a-zA-Z0-9\[\]])+$'
+
+    for i in range(n_boxes):
+        if int(d['conf'][i]) > 30:
+            if re.match(valid_pattern, d['text'][i]):
+                (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
+                image = cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    b,g,r = cv2.split(image)
+    rgb_img = cv2.merge([r,g,b])
+
+    plt.figure(figsize=(16,12))
+    plt.imshow(rgb_img)
+    plt.title('CONFIDENCE BOUNDING BOX RECOGNITION')
+    plt.show()
+    
 # grayscale
 def get_grayscale(image):
     return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
