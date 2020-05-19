f5e3cff11b4fc2ec4b3948c7e4aaa6e4bc99ed20
spencerwoo <spencer.woo@liferay.com>
2020-05-19 11:20:40 -0700

    drag move

--- _move.py
+++ _move.py
@@ -1,6 +1,7 @@
 import win32api, win32con
+import pyautogui
 
-from _settings import *
+from _configurations import *
 from _utils import *
 
 # Perhaps scroll screen via drag?
@@ -21,66 +22,78 @@ def _open_coordinates():
     _mouse_set(MAGNIFIER_ICON)
     _mouse_click()
 
+def _mouse_drag(start, end):
+    x1, y1 = start
+    x2, y2 = end
+    pyautogui.moveTo(x1, y1)
+    pyautogui.dragTo(x2, y2, .5)
+
 # =========
 # FUNCTIONS
 
-def button_magnify():
-    _open_coordinates()
+# def button_magnify():
+#     _open_coordinates()
 
-def button_go():
-    _mouse_set(COORDINATE_GO)
-    _mouse_click()
+# def button_go():
+#     _mouse_set(COORDINATE_GO)
+#     _mouse_click()
 
-def set_x(x):
-    _mouse_set(COORDINATE_X)
-    _mouse_click()
+# def set_x(x):
+#     _mouse_set(COORDINATE_X)
+#     _mouse_click()
 
-    calculator(x)
+#     calculator(x)
 
-def set_y(y):
-    _mouse_set(COORDINATE_Y)
-    _mouse_click()
+# def set_y(y):
+#     _mouse_set(COORDINATE_Y)
+#     _mouse_click()
 
-    calculator(y)
+#     calculator(y)
 
-def calculator_set_number(value):
-    pass
+# def calculator_set_number(value):
+#     pass
 
-def calculator(value):
+# def calculator(value):
 
-    print(value)
+#     print(value)
 
-    time.sleep(1)
-    # parse value into digits
-    # input digits calculator_set_number()
+#     # time.sleep(1)
+#     # parse value into digits
+#     # input digits calculator_set_number()
 
-    _mouse_set(CALCULATOR[10])
-    _mouse_click()
+#     _mouse_set(CALCULATOR[10])
+#     _mouse_click()
 
 # ===================
 # TOP LEVEL FUNCTIONS
 
-def drag_right():
-    return
-
+# midx = WINDOW_TOP_RIGHT[1][0] - 
 def drag_down():
     return
 
-def drag_left():
-    return
+def drag(option, base=False):
+    x = (WIDTH + 200 / 2) - 30
+    # if base:
+    #     x /= 2
+    #     y /= 2
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
 
-def set_coordinates(coordinates):
-    button_magnify()
+    _mouse_drag((MID_X, MID_Y), (MID_X - x, MID_Y))
 
-    time.sleep(1)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
 
-    set_x(coordinates[0])
-    time.sleep(1)
+# def set_coordinates(coordinates):
+#     button_magnify()
 
-    set_y(coordinates[1])
-    time.sleep(1)
+#     time.sleep(1)
 
-    button_go()
+#     set_x(coordinates[0])
+#     time.sleep(1)
+
+#     set_y(coordinates[1])
+#     time.sleep(1)
+
+#     button_go()
 
 def click_mouse_at(coordinates):
     _mouse_set(coordinates)
@@ -91,3 +104,5 @@ def click_mouse_at(coordinates):
 
 def move_mouse_at(coordinates):
     _mouse_set(coordinates)
+
+# def search_map(coordinates, )
